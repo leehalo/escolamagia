39b5750e5c839de8c3b3ec7c36af60c85d1f4276
leticia almeida <23143052@edu119.local>
2023-07-28 13:30:25 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+/*magia*/
+magia1= 'magia de fogo'
+magia2= 'magia de cura'
+magia3='magia de transformação'
+magia4= 'magia de ilusão'
+
+/*habilidades*/
+
+e='Controle de Elementos'
+f='Invocação de Criaturas Místicas'
+g='Leitura de Pergaminhos Antigos'
+h='Feitiços de Proteção'
+
+if (magia1,{
+
+}
